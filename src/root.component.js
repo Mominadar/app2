eb787d17b22ea71dbe516441b945da72a9305b81
Momina Babar <momina.babar@auxcube.com>
2024-07-24 15:59:45 +0500

    add nav

--- src/root.component.js
+++ src/root.component.js
@@ -1,3 +1,11 @@
+import * as singleSpa from "single-spa";
+
 export default function Root(props) {
-  return <section>{props.name} is mounted!</section>;
+  return <section>{props.name} is mounted!
+  <button onClick={()=>{
+    singleSpa.navigateToUrl("/app1");
+  }}>go to app1</button>
+  <button onClick={()=>{
+    singleSpa.navigateToUrl("/");
+  }}>go to dash</button></section>;
 }
